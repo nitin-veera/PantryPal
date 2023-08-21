ea2b455120b5d2baf82391eb24765339e15ac2f7
nitin-veera <nitinveera@ucla.edu>
2023-08-20 17:38:41 -0700

    Initial commit

---
+++ PantryPal/PantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalApp.swift
+//  PantryPal
+//
+//  Created by Nitin Veeraperumal on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
